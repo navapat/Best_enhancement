6583515c4a8fe072b9ce82a735700e13ab447cbc
omagiz01 <omagiz01@users.noreply.github.com>
2019-08-16 15:23:27 +0700

    add some HTML

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
-
+    return """
+        <!DOCTYPE html>
+        <html>
+        <body>
+        <h1>Hello World</h1>
+        <p>I'm Flask app</p>
+        <button type="button">Click Me!</button>
+        </body>
+        </html> """
 if __name__ == "__main__":
     app.run(debug = True)
